990efb929c93828099bdfd726ab6ee9ed728f6dd
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-02-17 13:25:09 +0530

    set CRUD

--- AngularCRUD/src/app/shared/property.service.ts
+++ AngularCRUD/src/app/shared/property.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,32 @@ export class PropertyService {
 
   constructor(private http:HttpClient) { }
   //Now Add Get, Post, Put and Delete method here
+  //Add property 
+  addListing(data:any){
+    return this.http.post('http://localhost:3000/properties', data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  //get All Property using get Methods
+
+  getAllProperty(){
+    return this.http.get('').pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  //Update property using put method
+  updateProp(data:any, id:number){
+    return this.http.put(''+id,data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+  //Delete property using delete method
+  deleteProp(id:number){
+    return this.http.delete(''+id).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
 }
