0e078ad318885696747473ed9b471029c5617ef6
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-02-17 15:05:39 +0530

    Update properties.component.ts

--- AngularCRUD/src/app/properties/properties.component.ts
+++ AngularCRUD/src/app/properties/properties.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { PropertyService } from '../shared/property.service';
 
 @Component({
   selector: 'app-properties',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./properties.component.css']
 })
 export class PropertiesComponent {
+allProperty:any;
+  constructor(private fb:FormBuilder, private api:PropertyService){}
+
+  ngOnInit():void{
+    this.api.getAllProperty()
+  }
+
+  getAllProperty(){
+    this.api.getAllProp().subscribe((res: any)=>{
+      this.allProperty = res;
+      console.warn(this.allProperty)
+    })
+  }
 
 }
